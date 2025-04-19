1046dbe8ca4fae3ef92b84aea155498c5f0bcec3
James Lawrence <jljatone@gmail.com>
2025-04-19 10:04:19 -0400

    ensure uploaded torrents receive a tracker if one is available

---
+++ shallows/internal/torrenttestx/torrenttestx.go
@@ -0,0 +1,27 @@
+package torrenttestx
+
+import (
+	"log"
+	"testing"
+
+	"github.com/james-lawrence/torrent"
+	"github.com/james-lawrence/torrent/dht"
+	"github.com/james-lawrence/torrent/dht/krpc"
+	"github.com/retrovibed/retrovibed/internal/testx"
+	"github.com/retrovibed/retrovibed/internal/torrentx"
+)
+
+func QuickClient(t testing.TB, options ...torrent.ClientConfigOption) *torrent.Client {
+	return testx.Must(testx.Must(torrentx.Autosocket(0))(t).Bind(
+		torrent.NewClient(
+			torrent.NewDefaultClientConfig(
+				torrent.ClientConfigPeerID(krpc.RandomID().String()),
+				torrent.ClientConfigSeed(true),
+				torrent.ClientConfigInfoLogger(log.New(log.Writer(), "[torrent] ", log.Flags())),
+				torrent.ClientConfigMuxer(dht.DefaultMuxer()),
+				torrent.ClientConfigBucketLimit(32),
+				torrent.ClientConfigCompose(options...),
+			),
+		),
+	))(t)
+}
