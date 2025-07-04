d8187c68d80bd31e23705393e0f42a732564bf9f
James Lawrence <james@egdaemon.com>
2025-07-04 11:21:07 -0400

    default verify_at to infinity

--- shallows/library/10_generate.genieql.go
+++ shallows/library/10_generate.genieql.go
@@ -1,5 +1,3 @@
 package library
 
-//go:generate which genieql
-//go:generate md5sum /home/jatone/go/bin/genieql
 //go:generate genieql auto -v -o "genieql.gen.go"
--- shallows/tracking/10_generate.genieql.go
+++ shallows/tracking/10_generate.genieql.go
@@ -1,5 +1,3 @@
 package tracking
 
-//go:generate which genieql
-//go:generate md5sum /home/jatone/go/bin/genieql
 //go:generate genieql auto -v -o "genieql.gen.go"
