d48f16381f3d1122472de0894f3d61fc0c838f37
James Lawrence <james@egdaemon.com>
2025-07-15 10:37:46 -0400

    remove remapping file system, torrent migration works better

--- shallows/cmd/cmdtorrent/cmdtorrent.go
+++ shallows/cmd/cmdtorrent/cmdtorrent.go
@@ -4,7 +4,6 @@ type Commands struct {
 	Verify     filesystemVerify `cmd:"" help:"verify torrents stored on disk and regenerate bitmaps"`
 	Import     importPeer       `cmd:"" help:"import torrents from a specific peer and a list of magnet urls"`
 	Export     exportMagnets    `cmd:"" help:"export seeded torrents as magnet urls to stdout or a file"`
-	Remap      remap            `cmd:"" help:"remaps symlinks to media by moving the files"`
 	Magnet     cmdMagnet        `cmd:"" help:"insert magnet links for download"`
 	KnownMedia cmdKnownMedia    `cmd:"" help:"best effort attempt to assign known media"`
 }
